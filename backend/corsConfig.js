b1c82e43efb1519de6455ac3145d89f4d0c98034
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-01-31 22:43:50 +0530

    some hope T_T

--- backend/corsConfig.js
+++ backend/corsConfig.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://election-poll-eight.vercel.app',
+    'https://shreya-mishra325-election-poll.vercel.app',
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
