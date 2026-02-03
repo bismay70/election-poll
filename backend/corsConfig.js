28ec5178e47245504f88f697b5357ccb823f1880
Shreya Mishra <shreyamishra2k05@gmail.com>
2026-02-03 18:59:03 +0530

    fixed scroll issue

--- backend/corsConfig.js
+++ backend/corsConfig.js
@@ -3,6 +3,7 @@ const corsOptions = {
     'http://localhost:5173',
     'http://localhost:3000',
     'https://shreya-mishra325-election-poll.vercel.app',
+    'https://shreya-mishra325-election-poll-bc4g6jz32.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
